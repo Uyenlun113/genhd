581c724ba1d6157681a69109b2f4e7d386dfdb81
Uyenlun113 <thiuyen1132004@gmail.com>
2026-08-05 09:11:26 +0700

    update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -85,6 +85,11 @@ export const authOptions: AuthOptions = {
     }),
   ],
   callbacks: {
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith('/')) return `${baseUrl}${url}`;
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
     async jwt({ token, user }) {
       if (user) {
         token.role = (user as any).role;
